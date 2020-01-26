d71b844e206db665821b1320496ac975057b06d7
JT <coreprogramm@gmail.com>
2020-01-26 23:47:45 +0530

    How to read values from appsettings.json in Asp.Net Core
    How to read values from appsettings.json in Asp.Net Core

---
+++ ReadAppsettings/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ReadAppsettings.Models;
+
+namespace ReadAppsettings.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<MailConfig> _config;
+        public HomeController(ILogger<HomeController> logger, IOptions<MailConfig> config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            string companyEmail = _config.Value.mailFrom;
+            string hostName = _config.Value.host;
+            string password = _config.Value.password;
+            int portNo = _config.Value.portNo;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ReadAppsettings/Controllers/MailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReadAppsettings.Models;
+
+namespace ReadAppsettings.Controllers
+{
+    public class MailController : Controller
+    {
+        private readonly MailConfig _config;
+        public MailController(MailConfig config)
+        {
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            string companyEmail = _config.mailFrom;
+            string hostName = _config.host;
+            string password = _config.password;
+            int portNo = _config.portNo;
+            return View();
+        }
+    }
+}
